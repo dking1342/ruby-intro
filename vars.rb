a7336929d5ecbe417621973b22413c87a0c583f6
David <leanmachine42@yahoo.com>
2022-07-20 22:52:29 -0500

    vars

---
+++ vars.rb
@@ -0,0 +1,13 @@
+# variables
+
+character_name = "kavooce"
+character_age = "30"
+
+puts ("There once was a man named " + character_name)
+puts ("he was " + character_age + " years old.")
+
+# change value of variable
+character_name = "jack"
+
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age + ".")
