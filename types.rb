45607dac1269e3cb195618c9b6bf9dc3b1200fd9
David <leanmachine42@yahoo.com>
2022-07-20 22:57:57 -0500

    types

---
+++ types.rb
@@ -0,0 +1,16 @@
+# basic data types
+
+# string
+name = "kavooce"
+
+# numbers and floats
+age = 25
+gpa = 3.2
+temp = -10
+
+# boolean
+is_loading = true
+is_active = false
+
+# nil is similar to null or nothing
+children = nil
