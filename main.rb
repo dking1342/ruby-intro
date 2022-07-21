256c1b42e547f8e60f84de3c78a87da7c15544af
David <leanmachine42@yahoo.com>
2022-07-20 22:40:15 -0500

    draw shape. use puts and print

--- main.rb
+++ main.rb
@@ -1,2 +1,17 @@
+# print to terminal 
+# puts and print will display to the terminal
+# puts displays the output along with a carriage return
+# print displays the output with no whitespace
 
-print "Hello World"
+# output text
+puts "hello!"
+print "kavooce"
+print " lives "
+print "in the usa"
+puts ""
+
+# drawing a shape using puts
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
