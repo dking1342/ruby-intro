d40c41965d7bf9af235eedc0ef070d12d235af50
David <leanmachine42@yahoo.com>
2022-07-21 15:16:24 -0500

    mad lib

---
+++ mad_libs.rb
@@ -0,0 +1,12 @@
+# mad libs game
+
+puts "Enter a color: "
+color = gets.chomp()
+puts "Enter a plural noun: "
+plural_noun = gets.chomp()
+puts "Enter a celebrity: "
+celebrity = gets.chomp()
+
+puts ("Roses are " + color)
+puts ("Violets are " + plural_noun)
+puts ("I love " + celebrity)
