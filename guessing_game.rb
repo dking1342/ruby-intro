69130f93c79838693dbd9654c53c8d116a4405e3
David <leanmachine42@yahoo.com>
2022-07-21 20:56:23 -0500

    guessing game

---
+++ guessing_game.rb
@@ -0,0 +1,23 @@
+# guessing game
+
+word = "banana"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
+
+while guess != word and !out_of_guesses
+  if guess_count < guess_limit
+    puts "Enter your guess: "
+    guess = gets.chomp().downcase()
+    guess_count += 1
+  else 
+    out_of_guesses = true  
+  end
+end
+ 
+if out_of_guesses
+  puts "ran out of guesses"
+else
+  puts "you won!"
+end
