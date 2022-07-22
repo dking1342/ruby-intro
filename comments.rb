9c966fe10d816b6bc38baead8172ce2205ed5b73
David <leanmachine42@yahoo.com>
2022-07-21 21:13:20 -0500

    comments

---
+++ comments.rb
@@ -0,0 +1,13 @@
+# comments
+
+# # is the symbol used to start the comment for a single line
+
+# comment can be put at the end of the line of code
+puts "comments are fun" # comment that describes this line
+
+
+# multiple lines of comment / comment block
+=begin
+line one
+line two
+=end
